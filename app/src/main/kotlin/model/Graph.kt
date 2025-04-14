f71353479b6ad956c55ad105fde295367433f35d
samodurov.nikita <nikita98351@gmail.com>
2025-04-14 19:21:14 +0300

    feat: implement directed graph

---
+++ app/src/main/kotlin/model/Graph.kt
@@ -0,0 +1,26 @@
+package model
+
+interface Graph<V, K> {
+    val vertices: Collection<Vertex<V>>
+    val edges: Collection<Edge<V, K>>
+
+    /**
+     * Adds edge to graph
+     *
+     * If edge already exist, overwrite it
+     *
+     * @param firstVertex First end point
+     * @param secondVertex Second end point
+     * @param key a key of edge
+     * @return Added edge
+     */
+    fun addEdge(firstVertex: V, secondVertex: V, key: K): Edge<V, K>
+    /**
+     * Adds vertex to graph
+     *
+     * If vertex already exist, overwrite it
+     *
+     * @param vertex A vertex to be added to a graph
+     */
+    fun addVertex(vertex: V)
+}
