602937c1a9114c8f87336e381f0884559a84d830
samodurov.nikita <nikita98351@gmail.com>
2025-04-21 18:56:34 +0300

    feat: make window draggable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import model.DirectedGraph
--- src/main/kotlin/view/DirectedGraphView.kt
+++ src/main/kotlin/view/DirectedGraphView.kt
@@ -3,12 +3,47 @@ package view
 import androidx.compose.runtime.Composable
 import viewmodel.DirectedGraphViewModel
 import VertexView
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
 
 @Composable
 fun <V,K> DirectedGraphView(
-    graphViewModel: DirectedGraphViewModel<K, V>
+    graphViewModel: DirectedGraphViewModel<K, V>,
 ) {
-    graphViewModel.edges.forEach { DirectedEdgeView(it, modifier = Modifier) }
-    graphViewModel.vertices.forEach { VertexView( it, modifier = Modifier) }
+    var modifier = Modifier
+        .pointerInput(Unit) {
+            detectDragGestures { change, delta ->
+                change.consume()
+                graphViewModel.vertices.forEach { vertex ->
+                    vertex.x += delta.x.dp
+                    vertex.y += delta.y.dp
+                }
+            }
+        }
+    graphViewModel.edges.forEach { DirectedEdgeView(it, modifier = modifier) }
+    graphViewModel.vertices.forEach { VertexView( it, modifier = modifier) }
 }
