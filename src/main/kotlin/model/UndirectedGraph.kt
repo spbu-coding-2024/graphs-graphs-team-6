869e4bc7d353cb0020865e91b76e8bbe3077a427
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-04-22 20:51:08 +0300

    feat: add validation for UndirectedEdge to require correct vertices pair

--- src/main/kotlin/model/UndirectedGraph.kt
+++ src/main/kotlin/model/UndirectedGraph.kt
@@ -6,11 +6,13 @@ class UndirectedGraph<V, K> : Graph<V, K> {
     data class UndirectedVertex<V>(override var element: V) : Vertex<V>
 
     data class UndirectedEdge<V, K>(
-        private val firstVertex: UndirectedVertex<V>,
-        private val secondVertex: UndirectedVertex<V>,
+        override val pair: Set<Vertex<V>>,
         override var key: K
     ) : Edge<V, K> {
-        override val pair: Collection<Vertex<V>> = setOf(firstVertex, secondVertex)
+        init {
+            require(pair.size == 2 || pair.size == 1)
+            { "Edge must connect 2 (or 1 if it is a loop) vertices" }
+        }
     }
 
     private val _vertices = HashMap<V, UndirectedVertex<V>>()
@@ -25,8 +27,7 @@ class UndirectedGraph<V, K> : Graph<V, K> {
     override fun addEdge(firstVertex: V, secondVertex: V, key: K): Edge<V, K> {
         return _edges.getOrPut(key) {
             UndirectedEdge<V, K>(
-                UndirectedVertex<V>(firstVertex),
-                UndirectedVertex<V>(secondVertex),
+                setOf(UndirectedVertex(firstVertex), UndirectedVertex(secondVertex)),
                 key
             )
         }
