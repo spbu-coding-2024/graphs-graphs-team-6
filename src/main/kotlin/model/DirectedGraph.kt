80800e5c26505bb8278be76d37396976349caf0d
pechenb <romkor2006@gmail.com>
2025-04-24 16:45:28 +0300

    feat: added adjacency list for vertices

--- src/main/kotlin/model/DirectedGraph.kt
+++ src/main/kotlin/model/DirectedGraph.kt
@@ -2,37 +2,36 @@ package model
 
 class DirectedGraph<V, K> : Graph<V, K> {
 
-    data class DirectedVertex<V>(override var element: V) : Vertex<V>
+	class DirectedVertex<V>(
+		override var element: V, override val adjacencyList: MutableList<Vertex<V>> = mutableListOf()
+	) : Vertex<V>
 
-    data class DirectedEdge<V, K>(
-        var firstVertex: DirectedVertex<V>,
-        var secondVertex: DirectedVertex<V>,
-        override var key: K
-    ) : Edge<V, K> {
-        override val pair: List<Vertex<V>> = listOf(firstVertex, secondVertex)
-    }
+	class DirectedEdge<V, K>(
+		var firstVertex: DirectedVertex<V>, var secondVertex: DirectedVertex<V>, override var key: K
+	) : Edge<V, K> {
+		override val pair: List<Vertex<V>> = listOf(firstVertex, secondVertex)
+	}
 
-    private val _vertices = HashMap<V, DirectedVertex<V>>()
-    private val _edges = HashMap<K, DirectedEdge<V, K>>()
+	private val _vertices = HashMap<V, DirectedVertex<V>>()
+	private val _edges = HashMap<K, DirectedEdge<V, K>>()
 
-    override val vertices: Collection<DirectedVertex<V>>
-        get() = _vertices.values
+	override val vertices: Collection<DirectedVertex<V>>
+		get() = _vertices.values
 
-    override val edges: Collection<DirectedEdge<V, K>>
-        get() = _edges.values
+	override val edges: Collection<DirectedEdge<V, K>>
+		get() = _edges.values
 
-    override fun addEdge(firstVertex: V, secondVertex: V, key: K): Edge<V, K> {
-        return _edges.getOrPut(key) {
-            DirectedEdge<V, K>(
-                DirectedVertex<V>(firstVertex),
-                DirectedVertex<V>(secondVertex),
-                key
-            )
-        }
-    }
+	override fun addEdge(firstVertex: V, secondVertex: V, key: K): Edge<V, K> {
+		val firstV  = _vertices.getOrPut(firstVertex)  { DirectedVertex(firstVertex) }
+		val secondV = _vertices.getOrPut(secondVertex) { DirectedVertex(secondVertex) }
+		firstV.adjacencyList.add(secondV)
 
-    override fun addVertex(vertex: V) {
-        _vertices.getOrPut(vertex) { DirectedVertex<V>(vertex) }
-    }
+		return _edges.getOrPut(key) { DirectedEdge(firstV, secondV, key) }
+	}
+
+
+	override fun addVertex(vertex: V) {
+		_vertices.getOrPut(vertex) { DirectedVertex<V>(vertex) }
+	}
 
 }
