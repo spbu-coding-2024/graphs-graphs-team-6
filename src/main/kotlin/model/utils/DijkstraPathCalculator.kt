d7c9792dfbadbc1bb3a08458d23a989ccdcc2c12
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-05-10 13:02:25 +0300

    refactor: memory to speed trade-off

--- src/main/kotlin/model/utils/DijkstraPathCalculator.kt
+++ src/main/kotlin/model/utils/DijkstraPathCalculator.kt
@@ -2,9 +2,26 @@ package model.utils
 
 import model.Edge
 import model.Graph
+import model.Vertex
 import java.util.PriorityQueue
 
 class DijkstraPathCalculator {
+
+    private fun <V, K, W : Comparable<W>> valueToVertexMap(graph: Graph<V, K, W>): Map<V, Vertex<V>> {
+        val result: MutableMap<V, Vertex<V>> = mutableMapOf()
+        for (vertex in graph.vertices) {
+            result[vertex.value] = vertex
+        }
+        return result
+    }
+
+    private fun <V, K, W : Comparable<W>> valuesToEdgeMap(graph: Graph<V, K, W>): Map<Pair<V, V>, Edge<V, K, W>> {
+        val result: MutableMap<Pair<V, V>, Edge<V, K, W>> = mutableMapOf()
+        for (edge in graph.edges) {
+            result[edge.startVertex.value to edge.endVertex.value] = edge
+        }
+        return result
+    }
 
     fun <V, K, W : Comparable<W>> runOn(
         graph: Graph<V, K, W>,
@@ -17,6 +34,9 @@ class DijkstraPathCalculator {
 
         val queue = PriorityQueue(compareBy<Pair<W, V>> { it.first })
 
+        val vertices = valueToVertexMap(graph)
+        val edges = valuesToEdgeMap(graph)
+
         distances[startVertex] = ring.zero
         queue.add(ring.zero to startVertex)
 
@@ -25,13 +45,13 @@ class DijkstraPathCalculator {
 
             if (!visited.add(currentVertexValue)) continue
 
-            val vertex = graph.vertices.firstOrNull { it.value == currentVertexValue } ?: continue
+            val vertex = vertices[currentVertexValue] ?: continue
 
             for (neighbor in vertex.adjacencyList) {
-                val edge = graph.edges.firstOrNull {
-                    (it.startVertex.value == currentVertexValue && it.endVertex.value == neighbor.value) ||
-                            (it.endVertex.value == currentVertexValue && it.startVertex.value == neighbor.value)
-                } ?: continue
+                val edge =
+                    edges[currentVertexValue to neighbor.value]?:
+                    edges[neighbor.value to currentVertexValue]?:
+                    continue
 
                 val weight = edge.weight
                 require(weight >= ring.zero) { "Edge weights must be non-negative" }
