b7783af4964176791d9ca6053215c7162aec5724
samodurov.nikita <nikita98351@gmail.com>
2025-05-29 10:52:56 +0300

    test: kamadakawai

---
+++ src/test/kotlin/model/KamadaKawaiTest.kt
@@ -0,0 +1,43 @@
+package model
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.unit.dp
+import org.junit.jupiter.api.RepeatedTest
+import view.MainScreenView
+import viewmodel.MainScreenViewModel
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+class KamadaKawaiTest {
+    val maxVertices = 30
+
+    @OptIn(ExperimentalTestApi::class)
+    @RepeatedTest(5)
+    fun `KamadaKawai algorithm converges on random graph when vertices are all appear on screen`() = runComposeUiTest {
+        val randomGraph = RandomUndirectedIntGraph.get(maxVertices)
+        val vm = MainScreenViewModel(randomGraph)
+        setContent {
+            MainScreenView(vm)
+        }
+        vm.kamadaKawai.drawGraph()
+
+    }
+
+    @OptIn(ExperimentalTestApi::class)
+    @RepeatedTest(5)
+    fun `KamadaKawai algorithm converges on random graph when vertices are not shown on screen`() = runComposeUiTest {
+        val randomGraph = RandomUndirectedIntGraph.get(maxVertices)
+        val vm = MainScreenViewModel(randomGraph)
+        for (vertex in vm.graphViewModel.vertices) {
+            vertex.x = -Random.nextInt(1000, 10000).dp
+            vertex.y = -Random.nextInt(1000, 10000).dp
+        }
+        setContent {
+            MainScreenView(vm)
+        }
+
+        vm.kamadaKawai.drawGraph()
+
+    }
+}
