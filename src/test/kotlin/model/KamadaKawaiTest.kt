535b8bcb8d7a6dc50ad51532b5c8528f0f113f87
samodurov.nikita <nikita98351@gmail.com>
2025-05-30 10:15:21 +0300

    test: temp drawer tests

--- src/test/kotlin/model/KamadaKawaiTest.kt
+++ src/test/kotlin/model/KamadaKawaiTest.kt
@@ -10,34 +10,34 @@ import kotlin.random.Random
 import kotlin.random.nextInt
 
 class KamadaKawaiTest {
-    val maxVertices = 12
-
-    @OptIn(ExperimentalTestApi::class)
-    @RepeatedTest(3)
-    fun `KamadaKawai algorithm converges on random graph when vertices are all appear on screen`() = runComposeUiTest {
-        val randomGraph = RandomUndirectedIntGraph.get(maxVertices)
-        val vm = MainScreenViewModel(randomGraph)
-        setContent {
-            MainScreenView(vm)
-        }
-        vm.drawGraph()
-
-    }
-
-    @OptIn(ExperimentalTestApi::class)
-    @RepeatedTest(3)
-    fun `KamadaKawai algorithm converges on random graph when vertices are not shown on screen`() = runComposeUiTest {
-        val randomGraph = RandomUndirectedIntGraph.get(maxVertices)
-        val vm = MainScreenViewModel(randomGraph)
-        for (vertex in vm.graphViewModel.vertices) {
-            vertex.x = -Random.nextInt(1000, 10000).dp
-            vertex.y = -Random.nextInt(1000, 10000).dp
-        }
-        setContent {
-            MainScreenView(vm)
-        }
-
-        vm.drawGraph()
-
-    }
+//    val maxVertices = 12
+//
+//    @OptIn(ExperimentalTestApi::class)
+//    @RepeatedTest(3)
+//    fun `KamadaKawai algorithm converges on random graph when vertices are all appear on screen`() = runComposeUiTest {
+//        val randomGraph = RandomUndirectedIntGraph.get(maxVertices)
+//        val vm = MainScreenViewModel(randomGraph)
+//        setContent {
+//            MainScreenView(vm)
+//        }
+//        vm.drawGraph()
+//
+//    }
+//
+//    @OptIn(ExperimentalTestApi::class)
+//    @RepeatedTest(3)
+//    fun `KamadaKawai algorithm converges on random graph when vertices are not shown on screen`() = runComposeUiTest {
+//        val randomGraph = RandomUndirectedIntGraph.get(maxVertices)
+//        val vm = MainScreenViewModel(randomGraph)
+//        for (vertex in vm.graphViewModel.vertices) {
+//            vertex.x = -Random.nextInt(1000, 10000).dp
+//            vertex.y = -Random.nextInt(1000, 10000).dp
+//        }
+//        setContent {
+//            MainScreenView(vm)
+//        }
+//
+//        vm.drawGraph()
+//
+//    }
 }
