d7747b4a7cd2bce0d5b2dffa62359cd5b8265b87
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-05-30 10:04:22 +0300

    feat: SQLite graph loader signature

--- src/main/kotlin/model/sqlite/GraphService.kt
+++ src/main/kotlin/model/sqlite/GraphService.kt
@@ -1,14 +1,22 @@
-package model.SQLite
+package model.sqlite
 
 import model.graph.Graph
 import java.sql.Connection
 
 object GraphService {
     fun <V, K, W : Comparable<W>> saveGraphToDatabase(graph: Graph<V, K, W>, connection: Connection) {
-        val vertexStmt = connection.prepareStatement("INSERT OR REPLACE INTO vertices (id, value) VALUES (?, ?)")
+        val id = 0
+        val orientation = 0
+        val weightType = 0
+        val vertexValueType = 0
+        val edgeKeyType = 0
+        val graphStmt = connection.prepareStatement("INSERT OR REPLACE INTO graphs (id, orientation, weightType, vertexValueType, edgeKeyType) VALUES ($id, $orientation, $weightType, $vertexValueType, $edgeKeyType)")
+        graphStmt.execute()
+
+        val vertexStmt = connection.prepareStatement("INSERT OR REPLACE INTO vertices (value, graph) VALUES (?, ?)")
         for (v in graph.vertices) {
             vertexStmt.setString(1, v.value.toString()) // assuming V has unique string
-            vertexStmt.setString(2, v.adjacencyList.toString())
+            vertexStmt.setString(2, id.toString())
             vertexStmt.addBatch()
         }
         vertexStmt.executeBatch()
@@ -24,4 +32,6 @@ object GraphService {
         edgeStmt.executeBatch()
     }
 
+    fun <V, K, W : Comparable<W>> loadGraphFromDatabase(graph: Graph<V, K, W>, connection: Connection) {
+    }
 }
