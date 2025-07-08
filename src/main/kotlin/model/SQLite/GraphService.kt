2e2f5cec87e7ac87dc9181a4e51370f2d7ab3a05
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-07-08 16:29:31 +0300

    feat: added sql graph saver

--- src/main/kotlin/model/SQLite/GraphService.kt
+++ src/main/kotlin/model/SQLite/GraphService.kt
@@ -1,14 +1,29 @@
 package model.SQLite
 
+import model.graph.DirectedGraph
 import model.graph.Graph
 import java.sql.Connection
 
 object GraphService {
-    fun <V, K, W : Comparable<W>> saveGraphToDatabase(graph: Graph<V, K, W>, connection: Connection) {
-        val vertexStmt = connection.prepareStatement("INSERT OR REPLACE INTO vertices (id, value) VALUES (?, ?)")
+    fun <V, K, W : Comparable<W>> saveGraphToDatabase(graph: Graph<V, K, W>, connection: Connection, name: String) {
+        val graphStmt = connection.prepareStatement("INSERT INTO graphs (name, V, K, W, directed) VALUES ($name, ?, ?, ?, ?)")
+
+        require(graph.vertices.last().value != null)
+        graphStmt.setString(1, graph.vertices.last().value!!::class.java.name)
+
+        require(graph.vertices.last().value != null)
+        graphStmt.setString(2, graph.edges.last().key!!::class.java.name)
+
+        graphStmt.setString(3, graph.edges.last().weight::class.java.name)
+
+        graphStmt.setString(4, (graph is DirectedGraph).toString())
+
+        val vertexStmt = connection.prepareStatement("INSERT OR REPLACE INTO vertices (id, graph, value, adj_list) VALUES (?, ?, ?, ?)")
         for (v in graph.vertices) {
-            vertexStmt.setString(1, v.value.toString()) // assuming V has unique string
-            vertexStmt.setString(2, v.adjacencyList.toString())
+            vertexStmt.setString(1, name + "_" + v.value.toString())
+            vertexStmt.setString(2, name)
+            vertexStmt.setString(3, v.value.toString())
+            vertexStmt.setString(4, v.adjacencyList.toString())
             vertexStmt.addBatch()
         }
         vertexStmt.executeBatch()
@@ -23,5 +38,4 @@ object GraphService {
         }
         edgeStmt.executeBatch()
     }
-
 }
