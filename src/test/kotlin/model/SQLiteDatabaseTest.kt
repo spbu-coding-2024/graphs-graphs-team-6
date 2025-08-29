33b22392e3c0af2bb176dc8cae1b2bec9d296f1f
RomanMaltsev <roman.maltsev.realgar@proton.me>
2025-08-29 14:28:42 +0300

    feat: add test ideas

---
+++ src/test/kotlin/model/SQLiteDatabaseTest.kt
@@ -0,0 +1,50 @@
+package model
+
+import org.junit.jupiter.api.Test
+
+class SQLiteDatabaseTest {
+    @Test
+    fun setMetadataValues(){
+        TODO()
+    }
+
+    @Test
+    fun setVertexValuesTest(){
+        TODO()
+    }
+
+    @Test
+    fun setEdgeValuesTest(){
+        TODO()
+    }
+
+    @Test
+    fun emptyGraph(){
+        TODO()
+    }
+
+    @Test
+    fun graphWithNoEdgesWriteRead(){
+        TODO()
+    }
+
+    @Test
+    fun simpleGraphWriteRead(){
+        TODO()
+    }
+
+    @Test
+    fun writeManyTimes(){
+        TODO()
+    }
+
+    @Test
+    fun readManyTimes(){
+        TODO()
+    }
+
+    @Test
+    fun writeReadManyTimes(){
+        TODO()
+    }
+}
