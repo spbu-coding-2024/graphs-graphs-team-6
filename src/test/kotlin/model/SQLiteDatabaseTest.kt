473c3b71fa2d2b5ebb1a4eba378f9226db0982b4
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-09-03 23:06:12 +0300

    test: add emptyGraph test

--- src/test/kotlin/model/SQLiteDatabaseTest.kt
+++ src/test/kotlin/model/SQLiteDatabaseTest.kt
@@ -1,50 +1,84 @@
 package model
 
+import model.graph.Graph
+import model.graph.UndirectedGraph
+import model.sqlite.SQLiteManager
+import model.sqlite.setupTables
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import space.kscience.kmath.operations.IntRing
+import java.sql.Connection
+import java.sql.DriverManager
 
+fun createMockConnection(): Connection {
+    val conn = DriverManager.getConnection("jdbc:sqlite::memory:")
+    setupTables(conn)
+
+    return conn
+}
+
+
+fun <V : Any, K : Any, W : Comparable<W>> isGraphsEqual(first: Graph<V, K, W>, second: Graph<V, K, W>): Boolean {
+    return first.vertices.all { firstVertex ->
+        second.vertices.any { secondVertex ->
+            firstVertex.value == secondVertex.value
+        }
+    } && second.vertices.all { secondVertex ->
+        first.vertices.any { firstVertex ->
+            firstVertex.value == secondVertex.value
+        }
+    } && first.edges.all { firstEdge ->
+        second.edges.any { secondEdge ->
+            firstEdge.startVertex == secondEdge.startVertex &&
+                    firstEdge.endVertex == secondEdge.endVertex &&
+                    firstEdge.key == secondEdge.key &&
+                    firstEdge.weight == secondEdge.weight
+        }
+    } && second.edges.all { secondEdge ->
+        first.edges.any { firstEdge ->
+            firstEdge.startVertex == secondEdge.startVertex &&
+                    firstEdge.endVertex == secondEdge.endVertex &&
+                    firstEdge.key == secondEdge.key &&
+                    firstEdge.weight == secondEdge.weight
+        }
+    } && first.vertices.size == second.vertices.size
+            && first.edges.size == second.edges.size
+}
+
+@DisplayName("Tests for SQLite database")
 class SQLiteDatabaseTest {
+    var manager = SQLiteManager(createMockConnection())
+
     @Test
-    fun setMetadataValues(){
+    fun emptyGraph() {
+        val graph = UndirectedGraph<Int, Int, Int>(IntRing)
+        manager.saveGraphToDatabase(graph, "emptyGraph")
+        val received = manager.loadGraphFromDatabase<Int, Int, Int>("emptyGraph")
+        assert(isGraphsEqual(received, graph))
+    }
+
+    @Test
+    fun graphWithNoEdgesWriteRead() {
         TODO()
     }
 
     @Test
-    fun setVertexValuesTest(){
+    fun simpleGraphWriteRead() {
         TODO()
     }
 
     @Test
-    fun setEdgeValuesTest(){
+    fun writeManyTimes() {
         TODO()
     }
 
     @Test
-    fun emptyGraph(){
+    fun readManyTimes() {
         TODO()
     }
 
     @Test
-    fun graphWithNoEdgesWriteRead(){
-        TODO()
-    }
-
-    @Test
-    fun simpleGraphWriteRead(){
-        TODO()
-    }
-
-    @Test
-    fun writeManyTimes(){
-        TODO()
-    }
-
-    @Test
-    fun readManyTimes(){
-        TODO()
-    }
-
-    @Test
-    fun writeReadManyTimes(){
+    fun writeReadManyTimes() {
         TODO()
     }
 }
