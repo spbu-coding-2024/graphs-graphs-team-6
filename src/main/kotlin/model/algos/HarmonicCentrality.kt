a39b15cd7bc0a16feae5f1f7468e9dd12b76c555
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-09-06 14:29:54 +0300

    fix: change centrality value type to Double, add filter to avoid counting distance from start to start

--- src/main/kotlin/model/algos/HarmonicCentrality.kt
+++ src/main/kotlin/model/algos/HarmonicCentrality.kt
@@ -3,17 +3,17 @@ package model.algos
 import model.graph.Graph
 
 class HarmonicCentrality<V : Any, K : Any, W : Comparable<W>> {
-    fun calculate(graph: Graph<V, K, W>): HashMap<V, W>{
-        val vertexCentrality: HashMap<V, W> = hashMapOf()
+    fun calculate(graph: Graph<V, K, W>): HashMap<V, Double> {
+        val vertexCentrality: HashMap<V, Double> = hashMapOf()
         for (startVertex in graph.vertices) {
             val (_, weights) = BellmanFordPathCalculator.bellmanFordAlgorithm(
                 graph,
                 startVertex.value
             )
-            vertexCentrality[startVertex.value] = graph.ring.zero
-            for (w in weights) {
-                //vertexCentralityMap[startVertex.value] = graph.ring.zero or partial sum
-                vertexCentrality[startVertex.value] = graph.ring.add(vertexCentrality[startVertex.value]!!, 1/w.value)
+            vertexCentrality[startVertex.value] = 0.0
+            for (w in weights.filter { it.key != startVertex.value }) {
+                //vertexCentralityMap[startVertex.value] = 0.0 or partial sum
+                vertexCentrality[startVertex.value] = vertexCentrality[startVertex.value]!! + 1.0 / w.value.toString().toDouble()
             }
         }
         return vertexCentrality
