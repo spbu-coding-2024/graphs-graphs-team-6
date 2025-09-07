2b857251b69916fa1862b9734f660205ecef9d06
Roman Maltsev <roman.maltsev.realgar@proton.me>
2025-09-07 23:59:51 +0300

    format: lint HarmonicCentralityTest.kt

--- src/test/kotlin/model/HarmonicCentralityTest.kt
+++ src/test/kotlin/model/HarmonicCentralityTest.kt
@@ -7,21 +7,23 @@ import space.kscience.kmath.operations.IntRing
 
 class HarmonicCentralityTest {
     @Test
-    fun emptyGraph(){
+    fun emptyGraph() {
         val graph: DirectedGraph<Int, Int, Int> = DirectedGraph(IntRing)
         val centralityMap = HarmonicCentrality<Int, Int, Int>().calculate(graph)
         assert(centralityMap.isEmpty())
     }
+
     @Test
-    fun singleVertexGraph(){
+    fun singleVertexGraph() {
         val graph: DirectedGraph<Int, Int, Int> = DirectedGraph<Int, Int, Int>(IntRing).apply {
             addVertex(0)
         }
         val centralityMap = HarmonicCentrality<Int, Int, Int>().calculate(graph)
         assert(centralityMap[0] == 0.0)
     }
+
     @Test
-    fun singleEdgeGraph(){
+    fun singleEdgeGraph() {
         val graph: DirectedGraph<Int, Int, Int> = DirectedGraph<Int, Int, Int>(IntRing).apply {
             addVertex(0)
             addVertex(1)
@@ -31,8 +33,9 @@ class HarmonicCentralityTest {
         assert(centralityMap[0] == 1.0)
         assert(centralityMap[1] == 0.0)
     }
+
     @Test
-    fun triangleGraph(){
+    fun triangleGraph() {
         val graph: DirectedGraph<Int, Int, Int> = DirectedGraph<Int, Int, Int>(IntRing).apply {
             addVertex(0)
             addVertex(1)
@@ -46,24 +49,26 @@ class HarmonicCentralityTest {
             assert(centralityMap[i] == 1.5)
         }
     }
+
     @Test
-    fun chainGraph(){
+    fun chainGraph() {
         val graph: DirectedGraph<Int, Int, Int> = DirectedGraph<Int, Int, Int>(IntRing).apply {
             for (i in 0..3) {
                 addVertex(i)
             }
-            for (i in 0..2){
-                addEdge(i, i+1, i, 1)
+            for (i in 0..2) {
+                addEdge(i, i + 1, i, 1)
             }
         }
         val centralityMap = HarmonicCentrality<Int, Int, Int>().calculate(graph)
-        assert(centralityMap[0] == 1.5 + 1.0/3.0)
+        assert(centralityMap[0] == 1.5 + 1.0 / 3.0)
         assert(centralityMap[1] == 1.5)
         assert(centralityMap[2] == 1.0)
         assert(centralityMap[3] == 0.0)
     }
+
     @Test
-    fun starGraph(){
+    fun starGraph() {
         val graph: DirectedGraph<Int, Int, Int> = DirectedGraph<Int, Int, Int>(IntRing).apply {
             for (i in 0..7) {
                 addVertex(i)
